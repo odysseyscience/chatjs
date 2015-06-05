5a23aae474fbe1c04713f34fce840adb9563da8d
Sean Adkinson <sean.adkinson@gmail.com>
2015-06-05 16:59:22 -0700

    Moved /lib to /src, and commonjs-ified jquery.autosize.js

---
+++ index.js
@@ -0,0 +1,7 @@
+
+var $ = require('jquery');
+require('./lib/js/jquery.autosize');
+require('./lib/js/jquery.chatjs.controller');
+require('./lib/css/jquery.chatjs.css');
+
+module.exports = $.chat.bind($);
